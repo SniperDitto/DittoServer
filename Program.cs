392c7b391724599aee480b25720b53728d6fd0c2
SniperDitto <sniperditto@kakao.com>
2023-03-31 00:26:57 +0900

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DittoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
