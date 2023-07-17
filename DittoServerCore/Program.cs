9dd6c4bb5f693b127c97c98249e6bebc1101a65b
yu_seunghwa <you_sunghwa@coconev.co.jp>
2023-07-17 17:08:16 +0900

    thread

--- DittoServerCore/Program.cs
+++ DittoServerCore/Program.cs
@@ -1,3 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread t = new Thread(Test);
+        t.Name = "thread 1";
+        t.IsBackground = true;//쓰레드 백드라운드 실행 여부(메인 끝나면 같이 끝남)
+        t.Start();
+        
+        Console.WriteLine("waiting for thread to end");
+        t.Join();//쓰레드가 끝날 때까지 대기
+        
+        Console.WriteLine("Hello");
+    }
+
+    static void Test()
+    {
+        while (true)
+        {
+            Console.WriteLine("Thread 1");
+
+        }
+    }    
+}
+
+
